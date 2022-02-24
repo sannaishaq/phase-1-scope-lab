ed00360a835c43d2ee04ad938c3c92682e61bb6c
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-24 09:38:47 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Max';
+function upperCaseCustomerName(){
+    customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Minnie';
+}
